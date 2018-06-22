263c08db29f1926f2e868f634d2946c47f9d4255
Michael Burt <michael@mpburt.com>
2018-06-22 13:08:59 -0600

    adding generic CAISO LMP collector

--- atlas/energy/caiso.py
+++ atlas/energy/caiso.py
@@ -113,24 +113,52 @@ class CaisoDaLmpPrc(BaseCaisoLmp):
     return self.data
     
 
-class CaisoRtLmpPrc(BaseCaisoLmp):
+class CaisoGenericLmp(BaseCaisoLmp):
   """
-  The url convention for this data is:
-    http://oasis.caiso.com/oasisapi/SingleZip?queryname=PRC_LMP&version=1   \
-      &startdatetime={YYYYMMDD}T07:00-0000                                  \
-      &enddatetime={YYYYMMDD}T07:00-0000                                    \
-      &market_run_id=RTM                                                    \
-      &resultformat=6                             << specifies a csv file
-      
-  - By default, just return LMP data. You can override this with the lmp_type 
-    kwarg and get MCC, MCE, or MCL LMP prices.
+  This is a generic LMP class for CAISO. The available LMP series are stored 
+  in cls.dataTypeMap(). 
+  
+  The constructor must include: datatype (str), date (datetime ob)
+  Output is a Pandas DataFrame
+  
+  You will get back LMP type data, which can be overridden with:
+    MCC, MCE, MCL LMP types
   """
   def __init__(self, lmp_type='LMP', **kwargs):
-    self.url = kwargs.get('url')
+    self.url = self.buildUrl()
+    self.datatype = kwargs.get('datatype')
+    self.date = kwargs.get('date').strftime('%Y%m%d')
     BaseCaisoLmp.__init__(self)
     self.filename = self.getFileName('PRC_LMP',lmp_type,'DAM')
-    self.datatype = 'DALMP_PRC'
-    self.collector = 'CaisoDaLmpPrc'
+  
+  def buildUrl(self):
+    meta = cls.dataTypeMap()
+    base = 'http://oasis.caiso.com/oasisapi/SingleZip?queryname='
+    url = base + '{0}&version=1&startdatetime={1}'.format(
+      self.datatype,
+      self.date)
+    url = url + '&enddatetime={0}&market_run_id={1}&resultformat=6'.format(
+      self.date + datetime.timedelta(days=1),
+      # add the appropriate market for the datatype
+      [d['market'] for d in meta if 
+        meta['atlas_datatype'] == self.datatype][0])
+    self.url = url
+    return self.url
+    
+  def dataTypeMap(cls):
+    datatypes = [{
+        'atlas_datatype':'HALMP_PRC',
+        'xml_name':'PRC_HASP_LMP',
+        'market':'HASP',
+        'xml_data_items':
+          ['LMP_CONG_PRC','LMP_ENE_PRC','LMP_LOSS_PRC','LMP_PRC','LMP_GHG_PRC']
+      },{
+        'atlas_datatype':'DALMP_PRC',
+        'xml_name':'PRC_LMP',
+        'market':'DAM',
+        'xml_data_items':
+          ['LMP_CONG_PRC','LMP_ENE_PRC','LMP_LOSS_PRC','LMP_PRC']
+      }]
     
   def getData(self, **kwargs):
     self.getFile()
